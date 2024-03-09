9b3fae87b273c14c0b1b90f72b8019f0f24275ec
Nicola Stack <nicolastack16@gmail.com>
2024-03-09 16:59:31 +0000

    add get user with bad id and deleted user tests

--- test/api/user-api-tests.js
+++ test/api/user-api-tests.js
@@ -33,4 +33,25 @@ suite("User API tests", () => {
     const returnedUser = await beachfinderService.getUser(testUsers[0]._id);
     assert.deepEqual(testUsers[0], returnedUser);
   });
+
+  test("get a user - bad id", async () => {
+    try {
+      await beachfinderService.getUser("1234");
+      assert.fail("Should not return a response");
+    } catch (error) {
+      assert(error.response.data.message === "No User with this id");
+      assert.equal(error.response.data.statusCode, 503);
+    }
+  });
+
+  test("get a user - deleted user", async () => {
+    await beachfinderService.deleteAllUsers();
+    try {
+      const returnedUser = await beachfinderService.getUser(testUsers[0]._id);
+      assert.fail("Should not return a response");
+    } catch (error) {
+      assert(error.response.data.message === "No User with this id");
+      assert.equal(error.response.data.statusCode, 404);
+    }
+  });
 });
